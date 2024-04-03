3caae2f9f050cd64ed2f76893f7ddb6f8979c0eb
Rsebs <sebastianruizj2014@gmail.com>
2024-04-02 23:57:08 -0500

    Detectar el color predominante

--- src/infrastructure/mappers/pokemon.mapper.ts
+++ src/infrastructure/mappers/pokemon.mapper.ts
@@ -1,10 +1,12 @@
+import { getColorFromImage } from '../../config/helpers/get-color';
 import { Pokemon } from '../../domain/entities/pokemon';
 import { PokeAPIPokemon } from '../interfaces/pokeApi.interfaces';
 
 export class PokemonMapper {
-  static pokeApiPokemonToEntity(data: PokeAPIPokemon): Pokemon {
+  static async pokeApiPokemonToEntity(data: PokeAPIPokemon): Promise<Pokemon> {
     const avatar = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
     const sprites = PokemonMapper.getSprites(data);
+    const color = await getColorFromImage(avatar);
 
     return {
       id: data.id,
@@ -12,6 +14,7 @@ export class PokemonMapper {
       avatar,
       sprites,
       types: data.types.map(type => type.type.name),
+      color,
     };
   }
 
