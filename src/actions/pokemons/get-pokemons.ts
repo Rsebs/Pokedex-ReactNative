b3277fd33bcce204be18f4d1deee115d4b2b1489
Rsebs <sebastianruizj2014@gmail.com>
2024-04-02 22:34:42 -0500

    Interfaces PokeApi

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,12 +1,29 @@
 import { pokeApi } from '../../config/api/pokeApi';
-import { Pokemon } from '../../domain/entities/pokemon';
+import type { Pokemon } from '../../domain/entities/pokemon';
+import type {
+  PokeAPIPaginatedResponse,
+  PokeAPIPokemon,
+} from '../../infrastructure/interfaces/pokeApi.interfaces';
 
-export const getPokemons = async (): Promise<Pokemon[]> => {
+export const getPokemons = async (
+  page: number,
+  limit: number = 20,
+): Promise<Pokemon[]> => {
   try {
     const url = '/pokemon';
-    const { data } = await pokeApi.get(url);
+    const { data } = await pokeApi.get<PokeAPIPaginatedResponse>(url, {
+      params: {
+        offset: page * 10,
+        limit,
+      },
+    });
+    const pokemonPromises = data.results.map(info =>
+      pokeApi.get<PokeAPIPokemon>(info.url),
+    );
 
-    console.log( data );
+    const pokeApiPokemons = await Promise.all(pokemonPromises);
+
+    console.log(pokeApiPokemons);
 
     return [];
   } catch (error) {
