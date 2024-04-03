6df9a5e07f29d4971e66a224246ae063d7196a69
Rsebs <sebastianruizj2014@gmail.com>
2024-04-02 23:32:46 -0500

    Diseño del HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,26 +1,42 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import { View } from 'react-native';
-import { ActivityIndicator, Button, Text } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { ActivityIndicator, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
+import { PokeballBg } from '../../components/ui/PokeballBg';
+import { FlatList } from 'react-native-gesture-handler';
+import { globalTheme } from '../../../config/theme/globalTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 export const HomeScreen = () => {
-  const { isLoading, data } = useQuery({
+  const { top } = useSafeAreaInsets();
+  const { isLoading, data: pokemons } = useQuery({
     queryKey: ['pokemons'], // Key de la petición que para que se maneje en caché
     queryFn: () => getPokemons(0), // La función encargada para realizar la petición
     staleTime: 1000 * 60 * 60, // 60 minutes | Mantiene la petición "Desactualizada" durante un tiempo, pasado el tiempo se realiza una petición nueva
   });
 
   return (
-    <View>
-      <Text variant="headlineLarge">HomeScreen</Text>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      )}
+    <View style={globalTheme.globalMargin}>
+      <PokeballBg style={styles.imgPosition} />
+
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{ paddingTop: top + 20 }}
+        ListHeaderComponent={() => <Text variant="displayMedium">Pokédex</Text>}
+        renderItem={({ item: pokemon }) => <PokemonCard pokemon={pokemon} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  },
+});
