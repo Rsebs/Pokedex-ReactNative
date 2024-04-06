cb17677fb584b8c4eeebca7c88c4a1447b418e6f
Rsebs <sebastianruizj2014@gmail.com>
2024-04-06 14:27:42 -0500

    SearchScreen - Diseño

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,17 +1,22 @@
 import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { FAB, Text, useTheme } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
 import { PokeballBg } from '../../components/ui/PokeballBg';
 import { FlatList } from 'react-native-gesture-handler';
 import { globalTheme } from '../../../config/theme/globalTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootStackParams } from '../../navigator/StackNavigator';
 
-export const HomeScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'HomeScreen'> {}
+
+export const HomeScreen = ({ navigation }: Props) => {
   const { top } = useSafeAreaInsets();
   const queryClient = useQueryClient();
+  const theme = useTheme();
 
   // Forma tradicional de una petición http
   // const { isLoading, data: pokemons = []} = useQuery({
@@ -48,6 +53,14 @@ export const HomeScreen = () => {
         onEndReachedThreshold={0.6}
         onEndReached={() => fetchNextPage()}
         showsVerticalScrollIndicator={false}
+      />
+
+      <FAB
+        label="Buscar"
+        style={[globalTheme.fab, { backgroundColor: theme.colors.primary }]}
+        mode="elevated"
+        color={theme.dark ? 'black' : 'white'}
+        onPress={() => navigation.push('SearchScreen')}
       />
     </View>
   );
--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import { globalTheme } from '../../../config/theme/globalTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ActivityIndicator, TextInput } from 'react-native-paper';
+import { Pokemon } from '../../../domain/entities/pokemon';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={[globalTheme.globalMargin, { paddingTop: top + 10 }]}>
+      <TextInput
+        placeholder="Buscar ¨Pokémon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={value => console.log(value)}
+        value={'hola mundo'}
+      />
+
+      <ActivityIndicator style={{ paddingTop: 20 }} />
+
+      <FlatList
+        data={[] as Pokemon[]}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{ paddingTop: top + 20 }}
+        renderItem={({ item: pokemon }) => <PokemonCard pokemon={pokemon} />}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
